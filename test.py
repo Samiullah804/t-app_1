6f2e56c6732d8f1029f97a74c119cb5e89f3b996
Samiullah Saleem <samiullahsmiles@gmail.com>
2025-02-21 11:02:54 +0500

    Update test.py

--- test.py
+++ test.py
@@ -122,16 +122,18 @@ hide_streamlit_style = """
 """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
-hide_streamlit_footer = """
+hide_streamlit_style = """
     <style>
         #MainMenu {visibility: hidden;}
         footer {visibility: hidden;}
         header {visibility: hidden;}
         .st-emotion-cache-1v0mbdj {display: none !important;} /* Hides "Created by ..." */
         .st-emotion-cache-16txtl3 {display: none !important;} /* Hides "Hosted with Streamlit" */
+        .st-emotion-cache-1wrcr25 {display: none !important;} /* Hides profile card */
+        .st-emotion-cache-1hynsf2 {display: none !important;} /* Hides other Streamlit elements */
     </style>
 """
-st.markdown(hide_streamlit_footer, unsafe_allow_html=True)
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 
 # Initialize session state
